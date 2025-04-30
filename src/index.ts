49faf5a4e26f49da175c957afd21ec3f5c39fcf4
Danny <danny.belmonte4@gmail.com>
2025-04-30 13:52:43 -0500

    fixed content-type for youtube pubsubhubbub

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ import connectDB from './config/database';
 import { errorMiddleware } from './middleware/error';
 import { rateLimiter }  from './middleware/rateLimit'; // Rate-limiting middleware
 import { config } from './config/env'; // Configuration settings
+import { setupScheduler } from './services/scheduler'; // Scheduler for periodic tasks
+import { subscribeToChannel } from './services/youtube';
 
 // connect to MongoDB
 connectDB()
@@ -14,12 +16,15 @@ connectDB()
 // Initialize Express app
 const app = express();
 
+app.set('trust proxy', 1); // trust first proxy (typical for Heroku/ngrok)
+
 // Middleware
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
-app.use(express.text({ type: 'application/xml' }));
+app.use(express.text({ type: 'application/atom+xml' }));
 app.use(errorMiddleware);
+
 // Apply rate-limiting to all routes except /api/webhooks
 app.use('/api', (req, res, next) => {
     if (req.path.startsWith('/webhooks')) {
@@ -37,3 +42,12 @@ const PORT = config.port || 3000;
 app.listen(PORT, () => {
     logger.info(`Server is running on port ${PORT}`);
 });
+
+
+
+
+(async () => {
+    logger.info('Subscribing to YouTube channel...');
+    await subscribeToChannel(config.youtubeChannelId, `${config.baseUrl}/api/webhooks/youtube`);
+    setupScheduler();
+})();
