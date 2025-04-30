cfa1565ea31d5737a669ce91975e0f97f3c3b6b1
Danny <danny.belmonte4@gmail.com>
2025-04-29 20:45:47 -0500

    updated process.env type

--- environment.d.ts
+++ environment.d.ts
@@ -1,11 +1,13 @@
 declare global {
     namespace NodeJS {
       interface ProcessEnv {
+        PORT: string;
+        MONGO_URI: string;
         YOUTUBE_API_KEY: string;
-        MONGODB_URI: string;
-        YOUTUBE_CHANNEL_ID: string;
         BASE_URL: string;
         WEBHOOK_SECRET: string;
+        MAX_RETRIES: string;
+        YOUTUBE_CHANNEL_ID: string;
       }
     }
   }
