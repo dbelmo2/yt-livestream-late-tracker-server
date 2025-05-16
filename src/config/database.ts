1a4ee20a2e93ba55a0fee731e216bc2bbe96e0e6
Danny <danny.belmonte4@gmail.com>
2025-05-16 15:26:45 -0500

    logs

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,6 +5,7 @@ import { config } from "./env";
 export default async () => {
     try {
         logger.info(`Connecting to MongoDB using URI "${config.mongoUri}"`);
+        logger.info(`process.env.MONGO_URI ${process.env.MONGO_URI}`);
         await mongoose.connect(config.mongoUri as string);
         logger.info("MongoDB connected successfully");
     }   catch (error) {
