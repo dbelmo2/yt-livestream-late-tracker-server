23cfeee8f76abee5ec87599e02c069d188154f30
Danny <danny.belmonte4@gmail.com>
2025-06-04 14:41:29 -0500

    back to 120hz

--- src/services/Match.ts
+++ src/services/Match.ts
@@ -53,7 +53,7 @@ export class Match {
   private readonly GAME_HEIGHT = 1080; // Fixed game height
   private readonly STARTING_X = 100;
   private readonly STARTING_Y = 100;
-  private readonly TICK_RATE = 60; // 60 ticks per second
+  private readonly TICK_RATE = 120; // 60 ticks per second
   private readonly MIN_MS_BETWEEN_TICKS = 1000 / this.TICK_RATE;
   private readonly MIN_S_BETWEEN_TICKS = this.MIN_MS_BETWEEN_TICKS / 1000; // Convert to seconds
   private readonly BUFFER_SIZE = 1024;
